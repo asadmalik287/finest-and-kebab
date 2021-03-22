f246b59bd8b9fa129a9d3b95feba6133b7cc9ec2
Asad Malik <74582564+asadmalik287@users.noreply.github.com>
2021-03-22 10:54:26 +0500

    first commit

---
+++ include/body-head.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="http://schema.org/WebPage">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <!-- Bootstrap CSS-->
+    <link href="assets/vendors/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome-->
+    <link rel="stylesheet" href="assets/vendors/font-awesome/css/font-awesome.min.css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries-->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file://-->
+    <!-- IE 9-->
+    <!-- Vendors-->
+    <link rel="stylesheet" href="assets/vendors/flexslider/flexslider.min.css">
+    <link rel="stylesheet" href="assets/vendors/swipebox/css/swipebox.min.css">
+    <link rel="stylesheet" href="assets/vendors/slick/slick.min.css">
+    <link rel="stylesheet" href="assets/vendors/slick/slick-theme.min.css">
+    <link rel="stylesheet" href="assets/vendors/animate.min.css">
+    <link rel="stylesheet" href="assets/vendors/bootstrap-datepicker/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="assets/vendors/pageloading/css/component.min.css">
+    <link rel="stylesheet" href="assets/vendors/dialog/css/dialog.css">
+    <!-- Font-icon-->
+    <link rel="stylesheet" href="assets/fonts/font-icon/style.css">
+    <!-- Style-->
+    <link rel="stylesheet" type="text/css" href="assets/css/layout.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/elements.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/extra.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/widget.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+    <!-- Google Font-->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700,700i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Rancho" rel="stylesheet">
+    <!-- Script Loading Page-->
+    <script src="assets/vendors/html5shiv.js"></script>
+    <script src="assets/vendors/respond.min.js"></script>
+    <script src="assets/vendors/pageloading/js/snap.svg-min.js"></script>
+    <script src="assets/vendors/pageloading/sidebartransition/js/modernizr.custom.js"></script>
+  </head>
+  <body>
+    <div id="pagewrap" class="pagewrap">
+      <div id="html-content" class="wrapper-content">
